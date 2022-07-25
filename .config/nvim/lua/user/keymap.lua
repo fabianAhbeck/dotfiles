84d4422151d72782c738e2787b5722ebf33d529e
Fabian Ahbeck <fabian.ahbeck@gmail.com>
2022-07-25 19:04:00 +0200

    Changed nvim config to lua

---
+++ .config/nvim/lua/user/keymap.lua
@@ -0,0 +1,44 @@
+local keymap = vim.api.nvim_set_keymap
+local term_opts = { silent = true }
+local opts = { noremap = true, silent = true }
+
+keymap("", "<Space>", "<Nop>", opts)
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
+keymap("i", "jj", "<ESC>", opts)
+
+keymap("n", "<leader>h", "<C-w>h", opts)
+keymap("n", "<leader>l", "<C-w>l", opts)
+keymap("n", "<leader>j", "<C-w>j", opts)
+keymap("n", "<leader>k", "<C-w>k", opts)
+
+keymap("n", "<leader>vs", ":vsplit<CR>", opts)
+keymap("n", "<C-Up>", ":resize +2<CR>", opts)
+keymap("n", "<C-Down>", ":resize -2<CR>", opts)
+keymap("n", "<C-Left>", ":vertical resize -2<CR>", opts)
+keymap("n", "<C-Right>", ":vertical resize +2<CR>", opts)
+
+keymap("n", "<leader>c", ":e $HOME/.config/nvim/init.lua<CR>", opts)
+
+keymap("n", "<S-l>", ":bnext<CR>", opts)
+keymap("n", "<S-h>", ":bprevious<CR>", opts)
+keymap("n", "<leader>nh", ":nohlsearch<CR>", opts)
+
+-- indent
+keymap("v", "<", "<gv", opts)
+keymap("v", ">", ">gv", opts)
+
+-- don't overwrite buffer on paste
+keymap("v", "p", '"_dP', opts)
+
+-- terminal --
+keymap("t", "<C-h>", "<C-\\><C-N><C-w>h", term_opts)
+keymap("t", "<C-j>", "<C-\\><C-N><C-w>j", term_opts)
+keymap("t", "<C-k>", "<C-\\><C-N><C-w>k", term_opts)
+keymap("t", "<C-l>", "<C-\\><C-N><C-w>l", term_opts)
+
+-- telescope
+keymap("n", "<leader>ff", "<cmd>lua require'telescope.builtin'.find_files(require('telescope.themes').get_dropdown({ previewer = false }))<cr>", opts)
+keymap("n", "<leader>fb", "<cmd>lua require'telescope.builtin'.buffers(require('telescope.themes').get_dropdown({ previewer = false }))<cr>", opts)
+keymap("n", "fg", "<cmd>Telescope live_grep<cr>", opts)
---
+++ .config/nvim/lua/user/lsp/installer.lua
@@ -0,0 +1,43 @@
+local on_attach = function(client, bufnr)
+  -- Mappings.
+  -- See `:help vim.lsp.*` for documentation on any of the below functions
+  local bufopts = { noremap=true, silent=true, buffer=bufnr }
+  vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
+  vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
+  vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
+  vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
+  vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, bufopts)
+  vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, bufopts)
+  vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, bufopts)
+  vim.keymap.set('n', '<space>wl', function()
+    print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+  end, bufopts)
+  vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, bufopts)
+  vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, bufopts)
+  vim.keymap.set('n', '<space>ca', vim.lsp.buf.code_action, bufopts)
+  vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
+  vim.keymap.set('n', '<space>f', vim.lsp.buf.formatting, bufopts)
+end
+
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
+
+local lspconfig = require('lspconfig')
+
+-- Enable some language servers with the additional completion capabilities offered by nvim-cmp
+local servers = { 'sumneko_lua', 'pyright' }
+for _, lsp in ipairs(servers) do
+	local opts = {
+    on_attach = on_attach,
+    capabilities = capabilities,
+	}
+
+  if lsp == "sumneko_lua" then
+  	local sumneko_opts = require("user.lsp.settings.sumneko_lua")
+  	opts = vim.tbl_deep_extend("force", sumneko_opts, opts)
+  end
+
+  lspconfig[lsp].setup {
+			opts
+  }
+end
