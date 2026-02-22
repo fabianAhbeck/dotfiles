f713497eacb8a36c87ad2faa4115fc47d66bbeea
Fabian Åhbeck <fabian.ahbeck@gmail.com>
2026-02-22 19:45:43 +0100

    chore: added go support

--- .config/nvim/lua/config/lsp-config.lua
+++ .config/nvim/lua/config/lsp-config.lua
@@ -2,49 +2,66 @@ vim.lsp.config('*', {
   root_markers = { '.git' },
 })
 
-vim.lsp.config.rust_analyzer = {
+local servers = {
+  rust_analyzer = {
     on_attach = on_attach,
     capabilities = capabilities,
     cmd = { 'rust-analyzer' },
     filetypes = { 'rust' },
-    root_markers = {"Cargo.toml", ".git"},
+    root_markers = { 'Cargo.toml', '.git' },
     single_file_support = true,
     settings = {
-        ['rust-analyzer'] = {
-            diagnostics = {
-                enable = false;
-            }
-        }
+      ['rust-analyzer'] = {
+        diagnostics = {
+          enable = false,
+        },
+      },
     },
     before_init = function(init_params, config)
-        -- See https://github.com/rust-lang/rust-analyzer/blob/eb5da56d839ae0a9e9f50774fa3eb78eb0964550/docs/dev/lsp-extensions.md?plain=1#L26
-        if config.settings and config.settings['rust-analyzer'] then
-            init_params.initializationOptions = config.settings['rust-analyzer']
-        end
+      -- See https://github.com/rust-lang/rust-analyzer/blob/eb5da56d839ae0a9e9f50774fa3eb78eb0964550/docs/dev/lsp-extensions.md?plain=1#L26
+      if config.settings and config.settings['rust-analyzer'] then
+        init_params.initializationOptions = config.settings['rust-analyzer']
+      end
     end,
+  },
+  lua_ls = {
+    cmd = { 'lua-language-server' },
+    filetypes = { 'lua' },
+    root_markers = { { '.luarc.json', '.luarc.jsonc' }, '.git' },
+    settings = {
+      Lua = {
+        runtime = {
+          version = 'LuaJIT',
+        },
+        diagnostics = {
+          -- Get the language server to recognize the `vim` global
+          globals = { 'vim' },
+        },
+        workspace = {
+          -- Make the server aware of Neovim runtime files and plugins
+          library = { vim.env.VIMRUNTIME },
+          checkThirdParty = false,
+        },
+      },
+    },
+  },
+  gopls = {
+    cmd = { 'gopls' },
+    filetypes = { 'go', 'gomod', 'gowork', 'gotmpl' },
+    root_markers = { 'go.work', 'go.mod', '.git' },
+    settings = {
+      gopls = {
+        completeUnimported = true,
+        usePlaceholders = true,
+        analyses = {
+          unusedparams = true,
+        },
+      },
+    },
+  },
 }
-vim.lsp.enable("rust_analyzer")
 
-
-vim.lsp.config['lua_ls'] = {
-  cmd = { 'lua-language-server' },
-  filetypes = { 'lua' },
-  root_markers = { { '.luarc.json', '.luarc.jsonc' }, '.git' },
-  settings = {
-    Lua = {
-      runtime = {
-        version = 'LuaJIT',
-      }
-    },
-    diagnostics = {
-      -- Get the language server to recognize the `vim` global
-      globals = { "vim" },
-    },
-    workspace = {
-      -- Make the server aware of Neovim runtime files and plugins
-      library = { vim.env.VIMRUNTIME },
-      checkThirdParty = false,
-    },
-  }
-}
-vim.lsp.enable("lua_ls")
+for name, config in pairs(servers) do
+  vim.lsp.config[name] = config
+  vim.lsp.enable(name)
+end
